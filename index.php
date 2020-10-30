be41c6fc4cc6f5efaf16bf4b496a8c9b41c08fe2
martykeyima <73689767+martykeyima@users.noreply.github.com>
2020-10-30 17:35:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
-<?php
-echo"<h2>My Name Is นวิน เวชสุวรรณ์</h2>";
-echo"<h2>Master M</h2>";
-echo"Commy<br>";
-?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello World</title>
+    </head>
+    <body>
+        <h1>My First Heading</h1>
+        <p>My name is นวิน เวชสุวรรณ์</p>
+        <img src = "comcat.jpg">
+        <img src = "comcatt.jpg">
+        <br>
+        <a href = "https://onlearn.it.kmitl.ac.th/course/view.php?id=228">This is a Link</a>
+
+        <h2>เรื่องตลก</h2>
+        <ul>
+            <li>คือตอนนี้ยังมีเวลามาตลกอีกเหรอครับ</li>
+            <li>ถามจริง?</li>
+            <li>คือเนื้อหาแน่นมากแล้วผมเป็นมือหม่ายยยยยย</li>
+        </ul>
+    </body>
+</html>
